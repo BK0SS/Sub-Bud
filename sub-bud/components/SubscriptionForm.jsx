561e1634a6d530c707cf889c949576618043dfa6
BK0SS <kosulin.b@mail.ru>
2026-03-14 16:31:00 -0700

    subform.jsx globals.css display

--- sub-bud/components/SubscriptionForm.jsx
+++ sub-bud/components/SubscriptionForm.jsx
@@ -1,5 +1,113 @@
-export default function SubscriptionForm(){
-    return (
-        <div></div>
-    )
+"use client";
+
+export default function SubscriptionForm() {
+  return (
+    <section>
+      <h2>Add new</h2>
+      <form onSubmit={() => {}}>
+        <label>
+          <span>Subscription name</span>
+          <input
+            type="text"
+            name="name"
+            placeholder="Netflix, Amazon, Spotify ..."
+            required
+          ></input>
+        </label>
+        <label>
+          <span>Category</span>
+          <select name="category" required>
+            {[
+              "Entertainment",
+              "Music",
+              "Software",
+              "Web Services",
+              "Health & Fitness",
+              "Food",
+              "Travel",
+              "Utility",
+              "Other",
+            ].map((cat, catIndex) => {
+              return <option key={catIndex}>{cat}</option>;
+            })}
+          </select>
+        </label>
+
+        <label>
+          <span>Cost</span>
+          <input type="number" name="cost" step="0.01"placeholder="9.99" required></input>
+        </label>
+
+        <label>
+            <span>Currency</span>
+            <select name="currency">
+                {['CAD','USD','EUR','RUB','UAH'].map((cur,curIndex)=>{
+                    return (
+                        <option key={curIndex}>
+                            {cur}
+                        </option>
+                    )
+                })}
+            </select>
+        </label>
+        <label>
+            <span>Billing frequency</span>
+            <select name="billingFrequency">
+                {['Monthly','Yearly','Quarterly','One-time'].map((bil,bilIndex)=>{
+                    return (
+                        <option key={bilIndex}>
+                            {bil}
+                        </option>
+                    )
+                })}
+            </select>
+        </label>
+
+        <label>
+            <span>Payment method</span>
+            <select name="paymentMethod">
+                {['Credit Card','Debit Card','Paypal','Bank Transfer','Other'].map((pay,payIndex)=>{
+                    return (
+                        <option key={payIndex}>
+                            {pay}
+                        </option>
+                    )
+                })}
+            </select>
+        </label>
+
+        <label>
+            <span>Subscription Start Date</span>
+            <input type="date" name="startDate" required></input>
+
+        </label>
+
+        <label>
+            <span>Status</span>
+            <select name="status">
+                {['Active','Paused','Cancelled'].map((sta,staIndex)=>{
+                    return (
+                        <option key={staIndex}>
+                            {sta}
+                        </option>
+                    )
+                })}
+            </select>
+        </label>
+
+        <label className="fat-column">
+            <span>Notes</span>
+            <textarea name="notes" placeholder="Leave a note to act as a reminder for yourself. For example 'Pause this subscription next month' or 'Shared with Girlfriend'"></textarea>
+        </label>
+        
+        <div className="fat-column form-submit-btns">
+            <button>Cancel</button>
+            <button type="submit">
+                Add
+            </button>
+        </div>
+
+      </form>
+    </section>
+  );
 }
--- sub-bud/components/SubscriptionsDisplay.jsx
+++ sub-bud/components/SubscriptionsDisplay.jsx
@@ -1,64 +1,77 @@
 import { getDaysUntilNextCharge, subscriptions } from "@/utils";
 
-export default function SubscriptionsDisplay(){
+export default function SubscriptionsDisplay() {
+  return (
+    <section>
+      <h2>Your Subscriptions</h2>
+      <div className="card-container">
+        {subscriptions.map((sub, subIndex) => {
+          const {
+            name,
+            category,
+            cost,
+            currency,
+            billingFrequency,
+            startDate,
+            notes,
+            status,
+          } = sub;
+          return (
+            <div key={subIndex} className="card subscription-card">
+              <div>
+                <h3>{name}</h3>
+                <div
+                  className={
+                    "status " +
+                    (status === "Active"
+                      ? " card-button-primary"
+                      : " card-button-secondary")
+                  }
+                >
+                  <small>{status}</small>
+                </div>
+              </div>
+              <p>
+                <i>{category}</i>
+              </p>
+              <div className="sub-cost">
+                <h2>${cost}</h2>
+                <p>{currency}</p>
+              </div>
+              <small>per {billingFrequency}</small>
+              <div className="sub-renewal">
+                <div>
+                  <p>Started</p>
+                  <h4>{startDate}</h4>
+                </div>
+                <div>
+                  <p>Due: {getDaysUntilNextCharge(startDate, billingFrequency)} days</p>
+                
+                </div>
 
-    return (
-        <section>
-            <h2>Your Subscriptions</h2>
-            <div className="card-container">
+              </div>
+              <div className="white-line"></div>
+                <p>{notes}</p>
 
-                {
-                    subscriptions.map((sub,subIndex)=>{
-                        const {name,category, cost, currency, billingFrequency,startDate,
-                            notes, status} =sub;
-                        return(
-                            <div key={subIndex} className="card subsription-card">
-                                <div>
-                                <h3>{name}</h3>
-                                <div className={'status ' + (status === 'Active' ? ' card-button-primary':' card-button-secondary')}>
-                                    <small>{status}</small>
-                                </div>
-                                </div>
-                                <p><i>{category}</i></p>
-                                <div className="sub-cost">
-                                    <h2>${cost}</h2>
-                                    <p>{currency}</p>
-                                </div>
-                                <small>per {billingFrequency}</small>
-                                <div className="sub-renewal">
-                                    <div>
-                                        <p>Started</p>
-                                        <h4>{startDate}</h4>
-                                    </div>
-                                    <div>
-                                        <p>Due</p>
-                                        <h4>{getDaysUntilNextCharge(startDate,billingFrequency)}</h4>
-                                    </div>
-                                    
-                                    <div className="white-line"></div>
-                                    <p>{notes}</p>
-                                    
-                                    <div className="subscription-actions">
-                                        <button className="button-card">
-                                            <i className="fa-solid fa-pen-to-square"></i>
-                                            Edit
-                                        </button>
-                                        <button className="button-card">
-                                            <i className="fa-solid fa-trash"></i>
-                                            Delete
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-
-                <button className="button-card add-subscriptions">
-                    <i className="fa-solid fa-plus"></i>
-                    <h5>Add new sbscription</h5>
-                </button>
+                <div className="subscription-actions">
+                  <button className="button-card">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                    Edit
+                  </button>
+                  <button className="button-card">
+                    <i className="fa-solid fa-trash"></i>
+                    Delete
+                  </button>
+                </div>
             </div>
-        </section>
-    )
+          );
+        })}
+
+        <button className="button-card add-subscription">
+          <i className="fa-solid fa-plus"></i>
+          <h5>Add new sbscription</h5>
+        </button>
+      </div>
+    </section>
+  );
 }
